376cfe3c71beb9abba07c7e5e73525e6ac2f31fd
unknown <youremail@example.com>
2026-06-25 15:18:48 +0530

    Fix OTP email failing on Render: force IPv4 for SMTP connection

--- server/utils/email.js
+++ server/utils/email.js
@@ -8,7 +8,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family:4
 });
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
